7dcee011a9ab27693925897a32f1661dca361444
HongDucNguyen <nduc@deakin.edu.au>
2018-07-19 13:11:43 +1000

    Initial check-in of module Wk1

---
+++ MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Wk1
+{
+    [Activity(Label = "Wk1", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+       // int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            ImageView img = FindViewById<ImageView>(Resource.Id.myImage);
+
+            var imgSwitch = true;
+            img.Click += delegate {
+                if (imgSwitch){
+                    int resourceId = (int)typeof(Resource.Drawable).GetField("light_off").GetValue(null);
+                    img.SetImageResource(resourceId);
+                    imgSwitch = false;
+                }
+                else{
+                    int resourceId = (int)typeof(Resource.Drawable).GetField("light_on").GetValue(null);
+                    img.SetImageResource(resourceId);
+                    imgSwitch = true;
+                }
+            };
+        }
+    }
+}
+
